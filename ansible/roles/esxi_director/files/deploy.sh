20619e3c313122c8992a7c6737e27fef1b7d4f9e
Binbin Zhao <bizhao@vmware.com>
2015-11-03 17:59:53 +0800

    tftp listens to all ip addresses

--- ansible/roles/esxi_director/files/deploy.sh
+++ ansible/roles/esxi_director/files/deploy.sh
@@ -3,11 +3,11 @@
 SRC=/tmp/tftp_config
 echo TFTP_USERNAME="\"tftp\"" > $SRC
 echo TFTP_DIRECTORY="\"/www/build/esxi/${esxi_build}/extract\"" >> $SRC
-echo TFTP_ADDRESS="\"10.111.90.122:69\"" >> $SRC
+echo TFTP_ADDRESS="\"[::]:69\"" >> $SRC
 echo TFTP_OPTIONS="\"--secure --verbose\"" >> $SRC
 
-echo "password" | sudo -S  cp $SRC /etc/default/tftpd-hpa
-echo "password" | sudo -S  service tftpd-hpa restart
+cp $SRC /etc/default/tftpd-hpa
+service tftpd-hpa restart
 
 for host in $target_hosts; do
     echo PXE-booting host $host
