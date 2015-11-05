4eba660f5873c91dd7a17de60d450c9b75b9d8a0
Binbin Zhao <bizhao@vmware.com>
2015-11-05 13:07:49 +0800

    add pxelinux.cfg/default for each esxi build

--- ansible/roles/esxi_director/files/extract_iso.sh
+++ ansible/roles/esxi_director/files/extract_iso.sh
@@ -11,3 +11,24 @@ cp -R /tmp/mnt/* extract/
 
 touch build.ready
 sudo umount /tmp/mnt
+
+# Create pxelinux.cfg folder and default file
+mkdir extract/pxelinux.cfg
+cd extract/pxelinux.cfg
+
+filename=$1
+VERSION_BUILD=${filename:25:13}
+
+echo "DEFAULT menu.c32
+MENU TITLE ESXi-${VERSION_BUILD}-full Boot Menu
+NOHALT 1
+PROMPT 0
+TIMEOUT 80
+LABEL install
+ KERNEL mboot.c32
+  APPEND -c boot.cfg
+MENU LABEL ESXi-${VERSION_BUILD} ^Installer
+LABEL hddboot
+LOCALBOOT 0x80
+MENU LABEL ^Boot from local disk
+" > default
